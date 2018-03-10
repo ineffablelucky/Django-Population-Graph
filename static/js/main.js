37e7ab26abb3c3d0be77bbdafa0a3b6e887b8bdc
Lakshay Kumar <33530741+ineffablelucky@users.noreply.github.com>
2018-03-10 21:05:31 +0530

    Add files via upload

---
+++ static/js/main.js
@@ -0,0 +1,19 @@
+$('.horizontal .progress-fill span').each(function(){
+  var percent = $(this).html();
+  $(this).parent().css('width', percent);
+});
+
+
+$('.vertical .progress-fill span').each(function(){
+  var percent = $(this).html();
+  var pTop = 100 - ( percent.slice(0, percent.length - 1) ) + "%";
+  $(this).parent().css({
+    'height' : percent,
+    'top' : pTop
+  });
+});
+
+$('.vertical .progress-fill').function myFunc(pop){
+  var x = document.getElementbyClassName("progress-fill").style.height
+  console.log("x")
+}
